79811d5611a4b4393eb3120c732fd1b38d2dc9b0
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-05-28 16:17:05 -0400

    Project created

---
+++ src/test/java/br/com/mfsdevsystem/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mfsdevsystem.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
