ebcbb45aaa821b37c0c7d31afbf4da9a6c63c51d
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-06-01 15:23:45 -0400

    OrderStatus enum

--- src/main/java/br/com/mfsdevsystem/course/config/TestConfig.java
+++ src/main/java/br/com/mfsdevsystem/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.mfsdevsystem.course.entities.Order;
 import br.com.mfsdevsystem.course.entities.User;
+import br.com.mfsdevsystem.course.enums.OrderStatus;
 import br.com.mfsdevsystem.course.repositories.OrderRepository;
 import br.com.mfsdevsystem.course.repositories.UserRepository;
 
@@ -30,13 +31,11 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null,"Alex Green", "alex@gmail.com", "97777777777", "123456");
 		User u3 = new User(null,"Marcelino Sousa", "marcelino@gmail.com", "98141-3390", "123456");
 		
-		
-		
-		Order o1 = new Order(null, Instant.parse("2024-06-01T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2024-06-01T08:20:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2024-06-01T10:53:01Z"), u1);
-		Order o4 = new Order(null, Instant.parse("2024-05-04T05:53:05Z"), u3);
-		Order o5 = new Order(null, Instant.parse("2024-05-04T06:53:04Z"), u3);
+		Order o1 = new Order(null, Instant.parse("2024-06-01T19:53:07Z"), OrderStatus.PAIND, u1);
+		Order o2 = new Order(null, Instant.parse("2024-06-01T08:20:10Z"), OrderStatus.WAITING_PAYMENT , u2);
+		Order o3 = new Order(null, Instant.parse("2024-06-01T10:53:01Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o4 = new Order(null, Instant.parse("2024-05-04T05:53:05Z"), OrderStatus.WAITING_PAYMENT, u3);
+		Order o5 = new Order(null, Instant.parse("2024-05-04T06:53:04Z"), OrderStatus.WAITING_PAYMENT, u3);
 		
 		userRepository.saveAll( Arrays.asList(u1, u2, u3 ));	
 		orderRepository.saveAll( Arrays.asList(o1, o2, o3, o4, o5 ));	
